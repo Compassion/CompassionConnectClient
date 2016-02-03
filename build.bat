7c661e056074a0f7ec254a31605f27d99657695a
Lucas Pickstone <lucasp@users.noreply.github.com>
2016-02-03 16:53:20 +1100

    Trying again to get NUnit 3 console runner to work.

--- build.bat
+++ build.bat
@@ -25,7 +25,7 @@ if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
 call %nuget% install NUnit.Runners -Version 3.0.1 -OutputDirectory packages
-packages\NUnit.Runners.3.0.1\tools\nunit-console.exe /config:%config% /framework:net-4.5 CompassionConnectClient.Tests\bin\%config%\CompassionConnectClient.Tests.dll
+packages\NUnit.Console.3.0.1\tools\nunit3-console.exe /config:%config% /framework:net-4.5 CompassionConnectClient.Tests\bin\%config%\CompassionConnectClient.Tests.dll
 if not "%errorlevel%"=="0" goto failure
 
 REM Package
